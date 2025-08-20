2de4ea8dd04804c3bd128eb853e8128d328d7d5f
vangal shravankumar <150109218+Shravan2134@users.noreply.github.com>
2025-08-20 17:40:44 +0530

    Update main.py

--- services/users/app/main.py
+++ services/users/app/main.py
@@ -4,8 +4,6 @@ from .models import User
 from .schemas import UserCreate, UserRead
 import os
 
-# just for testing
-
 DB_URL = os.getenv("DB_URL", "sqlite:///./users.db")
 connect_args = {"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
 engine = create_engine(DB_URL, connect_args=connect_args)
